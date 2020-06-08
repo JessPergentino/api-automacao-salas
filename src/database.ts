b2801d8f6dd0c3e6264866b82e5abf346826b528
JessPergentino <jessicapergentino@hotmail.com>
2020-06-07 21:05:02 -0300

    Alteração do banco

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ const sequelize = new Sequelize({
   database: "iot_bd",
   username: "postgres",
   password: "postgresql",
-  host: "localhost",
+  host: "192.168.1.67",
   dialect: "postgres",
 });
 
