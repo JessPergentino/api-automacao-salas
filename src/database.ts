ce4f080cbd3af918f0ce140a909c78d9f9671e3b
JessPergentino <jessicapergentino@hotmail.com>
2020-06-07 22:14:52 -0300

    Alteração host

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ const sequelize = new Sequelize({
   database: "iot_bd",
   username: "postgres",
   password: "postgresql",
-  host: "dbaf5aad0c1b.ngrok.io",
+  host: "192.168.1.67",
   dialect: "postgres",
 });
 
