64552d406937c7aba1e7d00580a9924e0045c45d
JessPergentino <jessicapergentino@hotmail.com>
2020-06-14 19:12:06 -0300

    Implementação da descriptografia das informações via rc4

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,17 +1,30 @@
 import { Request, Response } from "express";
 import Info from "../models/Info.model";
 
+import { rc4, tratarMensagem } from '../utils/rc4'
+
 class InfoController {
   static async salvarInfos(req: Request, res: Response): Promise<Response> {
-    console.log(req.params);
+    // console.log(req.params);
+    const luminosidade = tratarMensagem(req.params.luminosidade)
+    const temperatura = tratarMensagem(req.params.temperatura)
+    const lampada = tratarMensagem(req.params.lampada)
+    const arCondicionado = tratarMensagem(req.params.arCondicionado)
+    const pessoas = tratarMensagem(req.params.pessoas)
+
+    /* console.log(rc4("xxxxxxxxxxxxxxxxxxxxxx", luminosidade))
+    console.log(rc4("xxxxxxxxxxxxxxxxxxxxxx", temperatura))
+    console.log(rc4("xxxxxxxxxxxxxxxxxxxxxx", lampada))
+    console.log(rc4("xxxxxxxxxxxxxxxxxxxxxx", arCondicionado))
+    console.log(rc4("xxxxxxxxxxxxxxxxxxxxxx", pessoas)) */
 
     const info = new Info({
       sala: req.params.sala,
-      luminosidade: req.params.luminosidade,
-      temperatura: req.params.temperatura,
-      lampada: req.params.lampada === "1" ? true : false,
-      arCondicionado: req.params.arCondicionado === "1" ? true : false,
-      pessoas: req.params.pessoas === "1" ? true : false,
+      luminosidade: rc4("xxxxxxxxxxxxxxxxxxxxxx", luminosidade),
+      temperatura: rc4("xxxxxxxxxxxxxxxxxxxxxx", temperatura),
+      lampada: rc4("xxxxxxxxxxxxxxxxxxxxxx", lampada) === "1" ? true : false,
+      arCondicionado: rc4("xxxxxxxxxxxxxxxxxxxxxx", arCondicionado) === "1" ? true : false,
+      pessoas: rc4("xxxxxxxxxxxxxxxxxxxxxx", pessoas) === "1" ? true : false,
     });
 
     const infoCadastrada = await info.save();
@@ -20,7 +33,6 @@ class InfoController {
   }
 
   static async listarInfos(req: Request, res: Response): Promise<Response> {
-    console.log(req.params);
     const sala = req.params.sala;
 
     const infos = await Info.findAll({
